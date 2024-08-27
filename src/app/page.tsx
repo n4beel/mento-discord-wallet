f5f88f91d265e6c0b236d0c7bf06da6edbacbf4f
n4beel <nabeel3353045805@hotmail.com>
2024-08-27 17:08:26 +0500

    WalletConnect fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
-export function ConnectButton() {
-  return <w3m-button />
-}
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <ConnectButton />
+      <w3m-button />
     </main>
   );
 }
