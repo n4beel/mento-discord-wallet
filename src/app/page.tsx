084ed538526265e990d39107a94a2a5a9c1a6aaa
n4beel <nabeel3353045805@hotmail.com>
2024-09-09 17:50:49 +0500

    onesignal added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     // Ensure this code runs only on the client side
     if (typeof window !== 'undefined') {
       OneSignal.init({
-        appId: process.env.NEXT_ONESIGNAL_APP_ID || "",
-        safari_web_id: process.env.NEXT_ONESIGNAL_SAFARI_WEB_ID || "",
+        appId: process.env.NEXT_ONESIGNAL_APP_ID || "47d72783-3c4c-4a2e-b627-36600985e646",
+        safari_web_id: process.env.NEXT_ONESIGNAL_SAFARI_WEB_ID || "web.onesignal.auto.2ce6e088-bd06-4a6b-8c58-d83c247eb259",
         // You can add other initialization options here
         notifyButton: {
           enable: true,
